8e334644c3bcd43ce7bb2bc71248a238a0346569
95460eza <alfredkoffisokpa@gmail.com>
2023-09-13 22:49:04 -0400

    ADD CALLBACKS

--- app.py
+++ app.py
@@ -24,13 +24,39 @@ file_to_load = os.path.join( os.getcwd(), "dash_texts_data/df_airline_tweets.csv
 df = pd.read_csv(file_to_load)[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
 
 
+# SETUP CALLBACK: It Enables the use of control components for building the interaction
+@callback(
+    # You must provide all Outputs first. "component_property" are the parameters whose values the interaction will change
+    Output(component_id='graph-to-display', component_property='figure'),
+    Input(component_id='radio-button-to-select-column', component_property='value'),
+
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='airline', y=col_chosen, histfunc='avg')
+    return fig
+
+
 # Define the Web App Layout
-dash_app.layout = html.Div([
+dash_app.layout = dbc.Container([  dmc.Title('Welcome DATA Apprentice', color="blue", size="h3"),
+                                   dmc.RadioGroup( [dmc.Radio(i, value=i) for i in ['airline_sentiment_confidence','negativereason_confidence', 'retweet_count']],
+                                                   id='radio-button-to-select-column',
+                                                   value='retweet_count', size="sm"
+                                                 ),
+                                   dmc.Grid([  dmc.Col( [ dash_table.DataTable( data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'} ) ],
+                                                        span=6
+                                                        ),
+                                 dmc.Col([dcc.Graph(figure={}, id='graph-to-display')], span=6)
 
-                        # The html.Div() (note the ABSENCE of []) DASH COMPONENT with children= parameter adds TEXT to the webpage
-                        html.Div(children='Welcome DATA Apprentice')
+                             ]
 
-                        ])
+                             ),  # "," not needed
+
+                             ], fluid=True
+
+                            )
+
+
+
 
 if __name__ == '__main__':
     dash_app.run_server(debug=True)
