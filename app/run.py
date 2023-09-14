e5fdddd1f929983aee1dd667a94772bfb0448297
95460eza <alfredkoffisokpa@gmail.com>
2023-09-13 21:41:55 -0400

    changed app6 to app

--- app/run.py
+++ app/run.py
@@ -1,8 +1,8 @@
 from dashboard_web_app_definition_code import app
 
 if __name__ == '__main__':
-    app6.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
     
-    #app6.run(host='127.0.0.1', port=5000, debug=True)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
 
  
